3ff40f97be6c7fcdd9a489eb6d96b14b1ae19a06
shuo <shuoli84@gmail.com>
2023-02-13 11:51:17 +0800

    fix build for macos

--- build.sh
+++ build.sh
@@ -7,16 +7,20 @@ mkdir -p dis
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   objdump='llvm-objdump'
+  sym_prefix=''
+else
+  objdump='objdump'
+  sym_prefix='_'
 fi
 
 echo $objdump
 
-$objdump --disassemble-symbols=std_ahash_fixed --demangle target/release/hash_perf > dis/std_ahash_fixed
-$objdump --disassemble-symbols=std_ahash --demangle target/release/hash_perf > dis/std_ahash
-$objdump --disassemble-symbols=std_fxhash --demangle target/release/hash_perf > dis/std_fxhash
-$objdump --disassemble-symbols=hashbrown_13_ahash_fixed --demangle target/release/hash_perf > dis/hashbrown_13_ahash_fixed
-$objdump --disassemble-symbols=hashbrown_13_ahash --demangle target/release/hash_perf > dis/hashbrown_13_ahash
-$objdump --disassemble-symbols=hashbrown_13_fxhash --demangle target/release/hash_perf > dis/hashbrown_13_fxhash
-$objdump --disassemble-symbols=hashbrown_12_ahash_fixed --demangle target/release/hash_perf > dis/hashbrown_12_ahash_fixed
-$objdump --disassemble-symbols=hashbrown_12_ahash --demangle target/release/hash_perf > dis/hashbrown_12_ahash
-$objdump --disassemble-symbols=hashbrown_12_fxhash --demangle target/release/hash_perf > dis/hashbrown_12_fxhash
+$objdump --disassemble-symbols=${sym_prefix}std_ahash_fixed --demangle target/release/hash_perf > dis/std_ahash_fixed
+$objdump --disassemble-symbols=${sym_prefix}std_ahash --demangle target/release/hash_perf > dis/std_ahash
+$objdump --disassemble-symbols=${sym_prefix}std_fxhash --demangle target/release/hash_perf > dis/std_fxhash
+$objdump --disassemble-symbols=${sym_prefix}hashbrown_13_ahash_fixed --demangle target/release/hash_perf > dis/hashbrown_13_ahash_fixed
+$objdump --disassemble-symbols=${sym_prefix}hashbrown_13_ahash --demangle target/release/hash_perf > dis/hashbrown_13_ahash
+$objdump --disassemble-symbols=${sym_prefix}hashbrown_13_fxhash --demangle target/release/hash_perf > dis/hashbrown_13_fxhash
+$objdump --disassemble-symbols=${sym_prefix}hashbrown_12_ahash_fixed --demangle target/release/hash_perf > dis/hashbrown_12_ahash_fixed
+$objdump --disassemble-symbols=${sym_prefix}hashbrown_12_ahash --demangle target/release/hash_perf > dis/hashbrown_12_ahash
+$objdump --disassemble-symbols=${sym_prefix}hashbrown_12_fxhash --demangle target/release/hash_perf > dis/hashbrown_12_fxhash
